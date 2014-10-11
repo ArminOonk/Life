995a4abedad048016f90c88804261828048ea81b
Armin Oonk <sharpusenetbackup@aoonk.nl>
2014-10-11 17:51:50 +0200

    Added Color to life, hide the cursor

--- prog.c
+++ prog.c
@@ -112,9 +112,15 @@ bool readFile(bool *current, char *filename)
 
 int main(int argc, char **argv)
 {
-	initscr();										// start the curses mode
-	getmaxyx(stdscr,height,width);						// get the number of heights and widthumns
-	halfdelay(3);
+	initscr();						// start the curses mode
+	getmaxyx(stdscr,height,width);	// get the number of heights and widthumns
+	halfdelay(3);					// Set timeout on getch()
+	
+	start_color();					// Start color
+	init_pair(1, COLOR_RED, COLOR_WHITE);	// Create pair
+	attron(COLOR_PAIR(1));					// Set pair
+		
+	curs_set(0);							// Disable the cursor
 	
 	size = height*width;
 	
