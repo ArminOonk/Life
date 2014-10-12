df2c11457edcc8d65baa6602eeec0f552bca3b7f
Armin Oonk <sharpusenetbackup@aoonk.nl>
2014-10-12 16:44:40 +0200

    Move main to bottom the save space, placed all intitialisation in a define

--- prog.c
+++ prog.c
@@ -7,8 +7,8 @@
 #include <ctype.h>
 
 #define getIndex(x,y) (y*width + x)
-#define born(x,y) (getNrAlive(x, y) == 3)
-#define dies(x,y) (!(((unsigned int)getNrAlive(x, y)-2)<2))
+#define born(x,y) (getNrED(x, y) == 3)
+#define dies(x,y) (!(((unsigned int)getNrED(x, y)-2)<2))
 
 #define size (height*width)
 #define EXT ".life"
@@ -16,66 +16,26 @@
 
 #define P argv[0]
 
-#define SWAP previous = (bool*)((int)previous ^ (int)current); current = (bool*)((int)previous ^ (int)current); previous = (bool*)((int)previous ^ (int)current);
+#define S p = (bool*)((int)p ^ (int)c); c = (bool*)((int)p ^ (int)c); p = (bool*)((int)p ^ (int)c);
 #define COMMENT 0x21
 #define STOP 0x71 
 #define EMPTY 0x2e
-#define ALIVE 0x0a
+#define ED 0x0a
 #define DEAD 0x20
 
+#define D addch(DEAD
 #define MC memcpy((char*)argc
-
-int getNrAlive(int x, int y);
-bool readFile(char *filename);
-void updateLife(int x, int y);
-void printLife(int i);
+#define I initscr();getmaxyx(stdscr,height,width);halfdelay(1);start_color();init_pair(1, COLOR_RED, COLOR_WHITE);attron(COLOR_PAIR(1));curs_set(0);
 
 int height, width;	// to store the number of heights and the number of widthums of the screen
-bool *current, *previous;
+bool *c, *p;
 
-const char itString[] = "Iteration: %d";
+const char IS[] = "Iteration: %d";
 
-int main(int argc, char **argv)
-{		
-	initscr();								// start the curses mode
-	getmaxyx(stdscr,height,width);			// get the number of heights and widthumns
-	halfdelay(1);							// Set timeout on getch()
-	
-	start_color();							// Start color
-	init_pair(1, COLOR_RED, COLOR_WHITE);	// Create pair
-	attron(COLOR_PAIR(1));					// Set pair
-		
-	curs_set(0);							// Disable the cursor
-		
-	current = calloc(size<<1, sizeof(bool));// Create a double sized buffer  
-	previous = current+size;				// Set previous to half of the buffer
-	bool loadDemo = true;
-	
-	// Initial board
-	(argc > 1) ? (loadDemo = !readFile(argv[1])):0;
-	loadDemo ? (argc = (int)calloc( strlen(P) + EL + 1, 1), MC, P, strlen(P)), MC+strlen(P), EXT, EL), readFile((char*)argc)) : 0;
-	
-	printLife(0);
-	
-	while(getch() == ERR);// Wait for user to press a button
-	
-again:
-	SWAP
-	updateLife(0,0);
-	printLife(0);
-	
-	argc = getch();
-	if(argc == STOP || argc == (STOP-0x20))
-		goto stop;
-	goto again;
-stop:	
-	endwin();
-}
-
-int getNrAlive(int x, int y)
+int getNrED(int x, int y)
 {
-	int nrAlive;
-	(previous[getIndex(x, y)]) ? (nrAlive=-1) : (nrAlive=0);
+	int nrED;
+	(p[getIndex(x, y)]) ? (nrED=-1) : (nrED=0);
 	
 	for(int dy=(y-1); dy<= (y+1); dy++)
 	{
@@ -87,15 +47,15 @@ int getNrAlive(int x, int y)
 			(xt < 0) ? (xt = width -1) : ((xt >= width ) ? (xt = 0) : 0);
 			(yt < 0) ? (yt = height-1) : ((yt >= height) ? (yt = 0) : 0);
 
-			previous[getIndex(xt, yt)] ? nrAlive++:0;
+			p[getIndex(xt, yt)] ? nrED++:0;
 		}
 	}
-	return nrAlive;
+	return nrED;
 }
 
 void updateLife(int x, int y)
 {
-	previous[getIndex(x,y)] ? (dies(x, y) ? (current[getIndex(x,y)] = false) : (current[getIndex(x,y)] = true)) : (born(x, y) ? (current[getIndex(x,y)] = true) : (current[getIndex(x,y)] = false));
+	p[getIndex(x,y)] ? (dies(x, y) ? (c[getIndex(x,y)] = false) : (c[getIndex(x,y)] = true)) : (born(x, y) ? (c[getIndex(x,y)] = true) : (c[getIndex(x,y)] = false));
 	x++;
 	
 	(x >= width) ? (x = 0,	y++) : 0;
@@ -112,12 +72,12 @@ void printLife(int i)
 	if (i >= size)
 	{
 		static int iteration;
-		mvprintw(0,0, itString, iteration++);
+		mvprintw(0,0, IS, iteration++);
 		refresh();
 	}
 	else
 	{
-		i[current] ? addch(DEAD|ALIVE) : addch(DEAD);
+		i[c] ? D|ED) : D);
 		printLife(i+1);
 	}
 }
@@ -150,7 +110,7 @@ notEOF:
 		}
 		
 		startOfLine ? (startOfLine = false, (ch == COMMENT) ? skipLine = true : 0) : 0;
-		(ch == '\n') ? (y++, x=0, startOfLine = true, skipLine = false) : ((!skipLine)?	(current[getIndex(x,y)] = !(isspace(ch) || ch == EMPTY)), x++:0);
+		(ch == '\n') ? (y++, x=0, startOfLine = true, skipLine = false) : ((!skipLine)?	(c[getIndex(x,y)] = !(isspace(ch) || ch == EMPTY)), x++:0);
 		(x >= width) ? (x = 0, y++) : 0;
 		
 		if(y >= height)
@@ -163,4 +123,32 @@ notEOF:
 	fclose(readFP);
 	
 	return retVal;
+}
+
+int main(int argc, char **argv)
+{		
+	I	
+	c = calloc(size<<1, sizeof(bool));// Create a double sized buffer  
+	p = c+size;				// Set p to half of the buffer
+	bool loadDemo = true;
+	
+	// Initial board
+	(argc > 1) ? (loadDemo = !readFile(argv[1])):0;
+	loadDemo ? (argc = (int)calloc( strlen(P) + EL + 1, 1), MC, P, strlen(P)), MC+strlen(P), EXT, EL), readFile((char*)argc)) : 0;
+	
+	printLife(0);
+	
+	while(getch() == ERR);// Wait for user to press a button
+	
+again:
+	S
+	updateLife(0,0);
+	printLife(0);
+	
+	argc = getch();
+	if(argc == STOP || argc == (STOP-0x20))
+		goto stop;
+	goto again;
+stop:	
+	endwin();
 }
