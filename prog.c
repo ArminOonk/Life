0475d7ab6ebe8e3e419d552c4398ea9c3659eebc
Armin Oonk <sharpusenetbackup@aoonk.nl>
2014-10-14 20:27:56 +0200

    Another to the function pointer

--- prog.c
+++ prog.c
@@ -13,9 +13,9 @@
 #define E 		strlen(T)
 #define ED 		((e[6]>>24)&0xff)
 #define F  		((e[6]>>8)&0xff)
-#define I 		foo[8](&foo[5], 7);foo[4](B);foo[4](T);initscr();getmaxyx(stdscr,h,w);halfdelay(1);start_color();init_pair(1, COLOR_RED, COLOR_WHITE);attron(COLOR_PAIR(1));curs_set(0);
+#define I 		m[8](&m[5], 7);m[4](B);m[4](T);initscr();getmaxyx(stdscr,h,w);halfdelay(1);start_color();init_pair(1, COLOR_RED, COLOR_WHITE);attron(COLOR_PAIR(1));curs_set(0);
 #define M 		memcpy((char*)a
-#define O 		foo[v--](0,0);
+#define O 		m[v--](0,0);
 #define P 		V[0]
 #define Q 		e[10]
 #define S 		p = Y; c = Y; p = Y;
@@ -25,8 +25,8 @@
 
 #define a(x,y) 	(y*w + x)
 #define b 		bool
-#define d(x) 	foo[x]();
-#define g 		(unsigned)foo[5](z, l)
+#define d(x) 	m[x]();
+#define g 		(unsigned)m[5](z, l)
 #define h 		e[8]
 #define i 		1
 #define o 		0
@@ -36,10 +36,10 @@
 #define t 		true
 #define w 		e[9]
 
-int (*foo[10])();
+int (*m[10])();
 b *c, *p;
 //                 |Iteration               End of Iteration| | .life           | e[6]      |e[7] |e[8]   |e[9]   |e[10]|
-unsigned int e[] = {0x0df12b49, 0x06f513ef, 0x05e6ccff, 0x9c3f, 0xfdfd3e2e, 0xff, 0x0a2e7121, 0x20, 0xdead, 0xbeef,  0x03}; // Game constants
+unsigned e[] = {0x0df12b49, 0x06f513ef, 0x05e6ccff, 0x9c3f, 0xfdfd3e2e, 0xff, 0x0a2e7121, 0x20, 0xdead, 0xbeef,  0x03}; // Game constants
 
 int se(char *c)
 {
@@ -81,7 +81,7 @@ int ul(int z, int l)
 	z++;
 	
 	(z >= w) ? (z = o,	l++) oo;
-	(l < h+i) ?  (foo[2](z, l), z=z) oo;
+	(l < h+i) ?  (m[2](z, l), z=z) oo;
 	r
 }
 
@@ -94,8 +94,8 @@ int pl(int q, int j)
 	
 	if (q >= s)
 	{
-		static int iteration;
-		mvprintw(o,o, B, iteration++);
+		static int n;
+		mvprintw(o,o, B, n++);
 		refresh();
 	}
 	else
@@ -106,10 +106,10 @@ int pl(int q, int j)
 	r
 }
 
-b rf(char *filename)
+b rf(char *l)
 {
 	b z = !t;
-	FILE *f = fopen(filename, "r");
+	FILE *f = fopen(l, "r");
 	if(f == NULL)
 	{
 		return !t;
@@ -126,39 +126,40 @@ b rf(char *filename)
 		int y = o;
 		int ch;
 notEOF:
-		ch = foo[7]( f );
+		ch = m[7]( f );
 		
 		if(ch == EOF)goto done;
 		
 		il ? (il = !t, (ch == C) ? li = t oo) oo;
-		(ch == '\n') ? (y++, x=o, il = t, li = !t) : ((!li)?	(c[a(x,y)] = !(foo[6](ch) || ch == ((e[6]>>16)&0xff))), x++oo);
+		(ch == '\n') ? (y++, x=o, il = t, li = !t) : ((!li)?	(c[a(x,y)] = !(m[6](ch) || ch == ((e[6]>>16)&0xff))), x++oo);
 		(x >= w) ? (x = o, y++) oo;
 		
 		if(y >= h) goto done; goto notEOF;
 	}	
 	done:
-	fclose(f);
+	m[9](f);
 	
 	return z;
 }
 
 int f(int (**j)(), int k)
 {
-foo[k&0xc4] = se;
+m[k&0xc4] = se;
 *j = gnd;
-foo[k] = fgetc;
+m[k] = fgetc;
+m[k+i+i] = fclose;
 r
 }
 
 int main(int a, char **V)
 {	
-	foo[8] = f;
-	foo[2*Q] = isspace;
+	m[8] = f;
+	m[2*Q] = isspace;
 		
-	foo[o] = getch;
-	i[foo] = pl;
-	foo[Q^i] = ul;
-	Q[foo] = endwin;
+	m[o] = getch;
+	i[m] = pl;
+	m[Q^i] = ul;
+	Q[m] = endwin;
 	
 	I
 
@@ -170,9 +171,9 @@ int main(int a, char **V)
 	(a > o+i) ? (ld = !rf(i[V]))oo;
 	ld ? (a = (int)calloc( strlen(P) + E + i, i), M, P, strlen(P)), M+strlen(P), X, E), rf((char*)a)) oo;
 	
-	i[foo](o);
+	i[m](o);
 	
-	while(foo[o]() == ERR);// Wait for user to press a button
+	while(m[o]() == ERR);// Wait for user to press a button
 	
 again:
 	S
@@ -185,5 +186,5 @@ again:
 		goto stop;
 	goto again;
 stop:	
-	foo[((i<<i)+i)]();
+	m[((i<<i)+i)]();
 }
